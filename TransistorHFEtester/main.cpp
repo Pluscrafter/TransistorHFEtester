c2baeb86d6fd3e07ef7d0b3fb2ddf99199485b8c
Charpoan Kong <charpoan.kong@hotmail.com>
2018-11-29 11:49:20 +0100

    rm new 1.txt

--- TransistorHFEtester/main.cpp
+++ TransistorHFEtester/main.cpp
@@ -27,37 +27,32 @@ uint16_t hfe;
 
 int main(void)
 {
-	//TCCR0A = (1<<WGM00)|(1<<WGM01)|(1<<COM0A1);
+	TCCR0B = (1<<WGM00)|(1<<WGM01)|(1<<COM0B1);
 	
-	TCCR0A |= (1 << COM0A1) /*| (1 << WGM01) | (1 << WGM00)*/;
-	//PWM Prescaler / 1024
-	TCCR0B |= /*(1 << CS02) |*/ (1 << CS00);
-    /*ADMUX = (0<<REFS0)|(0<<REFS1)|(1<<MUX0);
-    ADCSRA = (1<<ADEN)|(1<<ADSC)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2);*/
+    ADMUX = (0<<REFS0)|(0<<REFS1)|(1<<MUX0);
+    ADCSRA = (1<<ADEN)|(1<<ADSC)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2);
 	
 	sei();
 	
-	//DDRB = (1<<PB1)|(0<<PB2);
-	//PORTB = (0<<PB1)|(1<<PB2);
+	DDRB = (1<<PB1)|(0<<PB2);
+	PORTB = (0<<PB1)|(1<<PB2);
 	
-	//pinMode(PB0, OUTPUT);
-	DDRB |= (1 << PB1);
 	
-	/*while(ADCSRA & (1<<ADSC)){
-	}*/
+	while(ADCSRA & (1<<ADSC)){
+	}
 	
     while (1) 
     {
-	/*ADCSRA |= (1<<ADSC);
-		while(ADCSRA & (1<<ADSC)){
+		ADCSRB |= (1<<ADSC);
+		while(ADCSRB & (1<<ADSC)){
 		}
 		
 		U3 = (Ucc/1024)*ADC; 
 		I2 = (U2-U3)/res[0];
 		I3 = (U3-UDiode)/res[1];
-		hfe = I2/I3;*/
+		hfe = I2/I3;
 		
-		OCR0A = 100;
+		OCR0B = 100;
 			
 		_delay_ms(1000);
     }
