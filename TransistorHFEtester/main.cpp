7ed43c359bd1839b4dec0e0792311684de03b12b
Charpoan Kong <charpoan.kong@hotmail.com>
2018-11-29 18:50:42 +0100

    ADD PWM

--- TransistorHFEtester/main.cpp
+++ TransistorHFEtester/main.cpp
@@ -7,7 +7,6 @@
 
 
 #include <avr/io.h>
-#include <avr/interrupt.h>
 
 #ifndef  F_CPU
 #define F_CPU 8000000UL
@@ -16,45 +15,44 @@
 #include <util/delay.h>
 #include <stdint.h>
 
-const float res[2] = {1000,100000};
-const float Ucc = 5.1;
-const float UDiode = 0.8;
-const float I1 = Ucc/res[0]+res[1]+res[1];
-const float U1 = I1*res[0];
-const float U2 = I1*(res[1]+res[2]);
-float U3,I2,I3;
+const double res[2] = {1000,100000};
+const double Ucc = 5.04;
+const double UDiode = 0.8;
+const double I1 = 0.000025075;
+const double U1 = 0.025074627;
+const double U2 = 2.532575;
+double U3,I2,I3;
 uint16_t hfe;
 
 int main(void)
 {
-	TCCR0B = (1<<WGM00)|(1<<WGM01)|(1<<COM0B1);
 	
+	TCCR0A |= (1<<WGM00)|(1<<WGM01)|(1<<COM0B1);
+	TCCR0B |= (1<<CS00);
     ADMUX = (0<<REFS0)|(0<<REFS1)|(1<<MUX0);
     ADCSRA = (1<<ADEN)|(1<<ADSC)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2);
 	
-	sei();
-	
-	DDRB = (1<<PB1)|(0<<PB2);
-	PORTB = (0<<PB1)|(1<<PB2);
-	
-	
+
+	OCR0B = 0x40;
+	DDRB |= (1 << DDB1);
 	while(ADCSRA & (1<<ADSC)){
 	}
 	
     while (1) 
     {
-		ADCSRB |= (1<<ADSC);
-		while(ADCSRB & (1<<ADSC)){
-		}
-		
+		ADCSRA |= (1<<ADSC);
+		while(ADCSRA & (1<<ADSC)){
+		}*
+		OCR0A = 0x40;
+		OCR0B = 0x40;
 		U3 = (Ucc/1024)*ADC; 
-		I2 = (U2-U3)/res[0];
-		I3 = (U3-UDiode)/res[1];
+		I2 = (U2-U3)/1000;
+		I3 = (U3-UDiode)/100000;
 		hfe = I2/I3;
 		
-		OCR0B = 100;
+		
 			
-		_delay_ms(1000);
+		//_delay_ms(1000);
     }
 	
 	return 0;
